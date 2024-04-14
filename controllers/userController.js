404111e48e516ae33fa6218f82c13d5df26daf12
Joe Andrews <joeandrews92@gmail.com>
2024-04-14 12:27:09 -0700

    Create userController functions with validation for signup_form

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,35 +1,81 @@
-/* const User = require("../models/user")
-const { body, validationResult } = require("express-validator");
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
-exports.user_signup_get = asyncHandler(async (req, res, next) => {
-    res.render("signup_form")
-});
 
-exports.user_signup_post = asyncHandler(async (req, res, next) => {
-    try {
-        const user = new User({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            username: req.body.username,
-            password: req.body.password
-        });
-        const result = await user.save();
-        res.redirect("/");
-      } catch(err) {
-        return next(err);
-      };
+
+exports.index = asyncHandler(async (req, res, next) => {
+    res.render('index', { title: 'Members Only' });
+
 })
 
-exports.user_login_get = asyncHandler(async (req, res, next) => {
-    res.render("login_form")
-});
+exports.signup_get = asyncHandler(async (req, res, next) => {
+    res.render("signup_form")
+})
 
-exports.user_login_post = asyncHandler(async (req, res, next) => {
+exports.signup_post = [
+    body("first_name", "First name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+    body("last_name", "Last name must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+    body("username", "Username must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+    body("password", "Password must be at least 8 characters long.")
+    .isLength({ min: 8 }),
+    body("confirm_password", "Passwords must match").custom((value, { req }) => {
+        return value === req.body.password;
+      }),
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.render("signup_form", {errors: errors.array() });
+        }
+        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+            if (err) {
+                console.log("Error hashing password:", err);
+                return next(err);
+            }
+            try {
+                const user = new User({
+                  first_name: req.body.first_name,
+                  last_name: req.body.last_name,
+                  username: req.body.username,
+                  password: hashedPassword,
+                });
+                const result = await user.save();
+                res.redirect("/");
+            } catch (err) {
+                console.log("Error saving user:", err)
+                return next(err);
+            }
+        });
+      })
+] 
+
+exports.login_get = asyncHandler(async (req, res, next) => res.render("login_form"))
+
+exports.login_post = asyncHandler(async (req, res, next) => {
     passport.authenticate("local", {
         successRedirect: "/",
-        failureRedirect: "/user/login"
-      })
+        failureRedirect: "/"
+    })(req, res, next); // Call the middleware here
 });
 
- */
+exports.logout_get = asyncHandler(async (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const bcrypt = require("bcryptjs");
-const User = require("../models/user");
 
+const user_controller = require("../controllers/userController")
 
-router.get("/", (req, res) => {
-  res.render('index', { title: 'Members Only' });
-});
+router.get("/", user_controller.index);
 
-router.get("/sign-up", (req, res) => res.render("signup_form"));
+router.get("/sign-up", user_controller.signup_get);
 
-router.post("/sign-up", async (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-      if (err) {
-          console.log(err);
-          return next(err);
-      }
-      try {
-          const user = new User({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            username: req.body.username,
-            password: hashedPassword,
-          });
-          const result = await user.save();
-          res.redirect("/");
-      } catch (err) {
-          return next(err);
-      }
-  });
-});
+router.post("/sign-up", user_controller.signup_post);
 
-router.get("/log-in", (req, res) => res.render("login_form"))
+router.get("/log-in", user_controller.login_get)
 
-router.post(
-  "/log-in",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/"
-  })
-);
+router.post("/log-in", user_controller.login_post)
 
-router.get("/log-out", (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
-});
+router.get("/log-out", user_controller.logout_get)
 
 module.exports = router;
