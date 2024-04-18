5c7c2f9c6053b7a5112b49ad962e81b5c5921398
Joe Andrews <joeandrews92@gmail.com>
2024-04-17 18:09:51 -0700

    Setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./views/*.ejs'],
+  content: ['./views/**/*.ejs'], 
   theme: {
     extend: {},
   },
-  plugins: [
-    {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  ],
-}
-
+  plugins: [],
+};
